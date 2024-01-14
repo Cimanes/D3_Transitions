b25d7a93da5848231ec6dfe0295c9fc00fb92631
Cimanes <cimanes75@telefonica.net>
2024-01-14 21:22:15 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 * Ref: 
 *  https://www.d3indepth.com/transitions/
 *  https://www.d3indepth.com/interaction/
-*  https://codepen.io/martinwantke/pen/rpNLWr
 */
 
 // Note: the array "data" contains the elements that define the locations and radii of the circles. 
@@ -13,7 +12,8 @@
 "use strict";
 const 
 	option = 2,				// Choose data format: (1) = Array of coordinates / (2) = Array of objects.
-	radius = 28,			// Maximum radius available (could be any property, such as population, amount...)
+	rMin = 2,					// Minimum radius available (could be any property, such as population, amount...)
+	rMax = 28,				// Maximum radius available (could be any property, such as population, amount...)
 	maxNumber = 20,		// Maximum number of circles in teh SVG container.
 	proximity = 30,		// Proximity to circle center to enable coloring.
 	delay = 1000,			// Delay during transition.
@@ -26,8 +26,20 @@ let data = [],			// Variable to store data from circles (position and radius).
 	width = window.innerWidth * hFactor, 		// Width of the SVG container.
 	height = window.innerHeight * vFactor; 	// Height of the SVG container.
 
-// Define the size of the svg container
-let box = d3.select('#box').style('width', width).style('height', height);
+
+let box = d3.select('#box')			// Define the size of the svg container
+	.style('width', width).style('height', height);
+
+// ================================================
+// Define and update the quadtree
+// ================================================
+let quadtree = d3.quadtree(); 	// Define the quadtree 
+if (option == 2) quadtree.x(d => d.x).y(d => d.y);	// req'd for option (2) only.
+
+function updateQuadtree() {	
+	quadtree.removeAll(quadtree.data());		// Start from empty quadtree.
+	quadtree.addAll(data);									// Create quadtree with all tne new data.
+}
 
 // ================================================
 // Re-define size of the container if the windown is resized
@@ -40,12 +52,6 @@ function rewindow(){
 window.addEventListener("resize", rewindow);
 
 // ================================================
-// Define the quadtree 
-// ================================================
-let quadtree = d3.quadtree(); 
-if (option == 2) quadtree.x(d => d.x).y(d => d.y);	// req'd for option (2) only.
-
-// ================================================
 // Function to update circles (number, x, y, r).
 // Two options depending on how the data array is configured:
 // ================================================
@@ -54,26 +60,25 @@ function updateData() {
 	let numPoints = Math.random() * maxNumber;	// Update number of items.
 	
 	for(let i=0; i<numPoints; i++) {						
-		
-		if (option == 1) 					// Option (1): arrays of type [x, y, r]. quadtree uses positions [0] & [1] for coordinates x & y
-			data.push( [
-				Math.random() * width, 			// data[i][0] is "x" coordinate within container (req'd for quadtree).
-				Math.random() * height, 		// data[i][1] is "y" coordinate within container (req'd for quadtree).
-				2 + Math.random() * radius 	// data[i][2] is radius.
-			] );
-		
-		else if (option == 2)			// Option (2): objects of type {id, x, y, r}. quadtree defines x y with item properties .x .y			
-			data.push( {										// Add items to the data array.
-				id: i,												// id of each circle.
-				x: Math.random() * width,			// "x" property is the "x" coordinate.
-				y: Math.random() * height,		// "y" property is the "y" coordinate.
-				r: 2 + Math.random() * radius	// "r" property is the "r" coordinate.
+		if (option == 1) 				// Option (1): arrays of type [x, y, r]. quadtree uses positions [0] & [1] for coordinates x & y
+			data.push( [											// Add items to the data array.
+				Math.random() * width, 					// data[i][0] is "x" coordinate within container (req'd for quadtree).
+				Math.random() * height, 				// data[i][1] is "y" coordinate within container (req'd for quadtree).
+				rMin + Math.random() * rMax 		// data[i][2] is radius (mi.
+			] );		
+		else if (option == 2)		// Option (2): objects of type {id, x, y, r}. quadtree defines x y with item properties .x .y			
+			data.push( {											// Add items to the data array.
+				id: i,													// id of each circle.
+				x: Math.random() * width,				// "x" property is the "x" coordinate.
+				y: Math.random() * height,			// "y" property is the "y" coordinate.
+				r: rMin + Math.random() * rMax	// "r" property is the "r" coordinate.
 			} );	 
 	}
 }
 
 // ================================================
 // Function to find top-left corner of an element.
+// Ref: https://codepen.io/martinwantke/pen/rpNLWr
 // ================================================
 function getOffset(element) {
   const bound = element.getBoundingClientRect();
@@ -91,47 +96,39 @@ function handleMousemove(e) {
 	const 
 	 	pos = d3.pointer(e, this),													// Define cursor position [x, y].
  		item = quadtree.find(pos[0], pos[1], proximity); 		// Find the closest element within proximity limit.
+	
 		updateTooltip(item);
 		updateCircles();																		// update the circles to change color if required.
 }
 
 // ================================================
-// Enable color changing when moving mouse within "box".
+// Enable user actions within "box".
 // ================================================
 function initEvents() { box.on('mousemove', handleMousemove); }
-
-// ================================================
-// Function to update quadtree
-// ================================================
-function updateQuadtree() {	
-	quadtree.removeAll(quadtree.data());		// Start from empty quadtree.
-	quadtree.addAll(data);									// Create quadtree with all tne new data.
-}
 
 // ================================================
 // Function to update tooltip when applicable 
 // ================================================
 function updateTooltip(item) {
-  const 
-		tooltip = d3.select('.tooltip'),
-		element = document.getElementById('box'),						// Define the svg box as "element"
-		offset = getOffset(element);												// Get the top and left offset of the svg box
+  const tooltip = d3.select('.tooltip');								// Define the tooltip element.
 	
 	hoveredId = item ? data.indexOf(item) : undefined;		// Index of closest circle (array of coordinates).
-  if (hoveredId === undefined) {
+  if (hoveredId === undefined) {												// Hide tooltip if cursor is not close to any circle.
     tooltip.style('opacity', 0);
-  } else {
-    const tipText = `id: ${hoveredId}<br>r = ${item.r.toFixed(2)}`;
-    const offset = getOffset(element);
-    const xTip = option == 1 ? item[0] : item.x;
-    const yTip = option == 1 ? item[1] : item.y;
-
-    tooltip
+  } 
+	else {																							// Show tooltip for the chosen circle.
+    const 
+			element = document.getElementById('box'),					// Define the svg box as "element".
+     	offset = getOffset(element),											// Get the top and left offset of the svg box.
+     	xTip = option == 1 ? item[0] : item.x,						// Define 'x' coordinate of the tooltip.
+     	yTip = option == 1 ? item[1] : item.y,						// Define 'y' coordinate of the tooltip.
+			tipText = `id: ${hoveredId}<br>r = ${item.r.toFixed(2)}`;		// Define text to be shown in tooltip.
+    tooltip																							// Define the tooltip.
       .style('opacity', 0.8)
       .style('width', tipWidth)
-      .html(tipText)
       .style('left', xTip + offset.left + 'px')
-      .style('top', yTip + offset.top + 'px');
+      .style('top', yTip + offset.top + 'px')
+			.html(tipText);
   }
 }
 
@@ -146,7 +143,6 @@ function updateCircles() {
 				return enter
 					.append('circle')												// append a circle for each element in "data".
 					.attr('cy', d => option == 1 ? d[1] : d.y );		// Initial "y" coordinate for transition (x and r are 0 by default). 
-					// .style('fill', (d, i) => (i === hoveredId ? 'red' : null));
 			},
 			function(update) { return update; },				// define what happens to existing elements.
 			function(exit) { 														// Define how "exiting" elements disappear.
